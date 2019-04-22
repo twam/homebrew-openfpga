9c6d801e6beed3d581acc9cc6792bce558d981a6
Tobias Müller <Tobias_Mueller@twam.info>
2019-04-22 17:52:00 +0200

    Make prjtrellis pass audit and use Ninja

--- Formula/prjtrellis.rb
+++ Formula/prjtrellis.rb
@@ -2,27 +2,31 @@ class Prjtrellis < Formula
   desc "Device database and tools for bitstream creation for ECP5 FPGAs"
   homepage "https://github.com/SymbiFlow/prjtrellis"
   url "https://github.com/SymbiFlow/prjtrellis/archive/1.0.tar.gz"
-  version "1.0"
   sha256 "6cfa12b7bf1ad5aed2b711fdfdd5ade6a2047f4733a704cb0b04634d350a4e26"
   head "https://github.com/SymbiFlow/prjtrellis.git"
 
   depends_on "cmake" => :build
+  depends_on "ninja" => :build
   depends_on "boost"
   depends_on "boost-python3"
-  depends_on "python@3"
+  depends_on "python"
 
   resource "database" do
-    url "https://github.com/SymbiFlow/prjtrellis-db/archive/d0b219af41ae3da6150645fbc5cc5613b530603f.zip"
+    url "https://github.com/SymbiFlow/prjtrellis-db/archive/d0b219af41ae3da6150645fbc5cc5613b530603f.tar.gz"
     sha256 "dd85dfb7a8606fa490cfcc4472f6d274b1b00d76a4e18e7669ecc59dc6997082"
   end
 
   def install
-    (buildpath/"database").install resource("database") if not build.head?
+    (buildpath/"database").install resource("database") unless build.head?
 
     cd "libtrellis" do
-      system "cmake", *std_cmake_args
-      system "make"
-      system "make install"
+      system "cmake", "-GNinja", *std_cmake_args
+      system "ninja"
+      system "ninja", "install"
     end
+  end
+
+  test do
+    system "#{bin}/ecppack", "--help"
   end
 end
