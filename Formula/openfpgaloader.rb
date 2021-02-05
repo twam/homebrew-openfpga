99bf3d748031638b8b61a48f3dbc564a6ae310d9
Tobias Müller <Tobias_Mueller@twam.info>
2021-02-05 19:25:34 +0100

    Add openFPGAloader

---
+++ Formula/openfpgaloader.rb
@@ -0,0 +1,22 @@
+class Openfpgaloader < Formula
+  desc "Universal utility for programming FPGA"
+  homepage "https://github.com/trabucayre/openFPGALoader"
+  url "https://github.com/trabucayre/openFPGALoader/archive/v0.2.1.zip"
+  version "0.2.1"
+  sha256 "8f1de525b61d1f70f19ddb7158f7d1e8d9f55caf597930f8437e82c11c772255"
+  head "https://github.com/trabucayre/openFPGALoader.git"
+
+  depends_on "libftdi"
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+
+  def install
+    system "cmake", ".", "-GNinja", *std_cmake_args
+    system "ninja"
+    system "ninja", "install"
+  end
+
+  test do
+    system "#{bin}/openFPGALoader"
+  end
+end
