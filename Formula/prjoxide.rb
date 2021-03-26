4770b9282e6f579d8a4271380911d821c7ca8881
Tobias Müller <Tobias_Mueller@twam.info>
2021-03-26 10:49:35 +0100

    Update prjoxide to 20210325

--- Formula/prjoxide.rb
+++ Formula/prjoxide.rb
@@ -1,16 +1,16 @@
 class Prjoxide < Formula
   desc "Device database and tools for bitstream creation for Lattice Nexus FPGAs"
-  homepage "https://github.com/daveshah1/prjoxide"
-  version "20201207"
-  url "https://github.com/daveshah1/prjoxide/archive/09d9d16e25e920f550c3c3bfaeaaaeca4e7f24c4.tar.gz"
-  sha256 "7807cd58170a0110cae6efc76afd73f1de2081ad5ede5fd882ec2cf661beeea4"
-  head "https://github.com/daveshah1/prjoxide.git"
+  homepage "https://github.com/gatecat/prjoxide"
+  version "20210325"
+  url "https://github.com/gatecat/prjoxide/archive/7dd6fecac3b19294e2b82b07980c279bca74c46b.tar.gz"
+  sha256 "a62dbd3e69d1c9e9af89390a0240872eb50e85469105ed6648ee1aa2839ae0f1"
+  head "https://github.com/gatecat/prjoxide.git"
 
   depends_on "rust" => :build
 
   resource "database" do
-   url "https://github.com/daveshah1/prjoxide-db/archive/9726bc867ec77319e11086552072400d13fe296d.tar.gz"
-   sha256 "da0f0049d8258f7fcb37af5d55f5092bf0c4a85ac164dad6e95c10ed16a5c2cb"
+   url "https://github.com/gatecat/prjoxide-db/archive/5427616ec04f25f50a200060116c4b41fcfe7fbe.tar.gz"
+   sha256 "fba943d3e3b4531976f06e1671d1cc84608a1d5e4725ffda4cd5cf10ccb60981"
   end
 
   def install
