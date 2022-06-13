2fea9d2a7f34075ba202fb77307bb6dbc36ccb9f
Tobias Müller <Tobias_Mueller@twam.info>
2022-06-13 08:24:35 +0200

    Update ecpprog to 20211204

--- Formula/ecpprog.rb
+++ Formula/ecpprog.rb
@@ -1,9 +1,9 @@
 class Ecpprog < Formula
   desc "A basic driver for FTDI based JTAG probes, to program ECP5 FPGAs."
   homepage "https://github.com/gregdavill/ecpprog"
-  url "https://github.com/gregdavill/ecpprog/archive/d654a4d0ad9ad794c24af2079000b813e204eecf.tar.gz"
-  version "20200920"
-  sha256 "4ff36b6896ea9d9b5e940e34069160d228f13b7a912ce63fb3bb5a5fe436abb1"
+  url "https://github.com/gregdavill/ecpprog/archive/7212b56a9d2fc6de534e06636a1c6d8b0c6f80ab.tar.gz"
+  version "20211204"
+  sha256 "06b08cad1b039aa3ef119c6057b613712476fe6c8ef96fa494c4d4a7dec34c65"
   head "https://github.com/gregdavill/ecpprog.git"
 
   depends_on "libftdi0"
