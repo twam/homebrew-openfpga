6a2eed855fdc3994f0af49c47e11e19b4202973f
Tobias Müller <Tobias_Mueller@twam.info>
2022-06-13 08:22:33 +0200

    Update icestorm to 20220603

--- Formula/icestorm.rb
+++ Formula/icestorm.rb
@@ -1,9 +1,9 @@
 class Icestorm < Formula
   desc "Bitstream Tools for Lattice iCE40 FPGAs"
   homepage "http://www.clifford.at/icestorm/"
-  url "https://github.com/YosysHQ/icestorm/archive/3b7b1991318860997ef589112b3debb24eb4912d.tar.gz"
-  version "20220102"
-  sha256 "ed34e5c79bb4ccc36673f39abd68eea6d2d0feb49222a61e0636555fc201c5ea"
+  url "https://github.com/YosysHQ/icestorm/archive/2bc541743ada3542c6da36a50e66303b9cbd2059.tar.gz"
+  version "20220603"
+  sha256 "3e47b12a7d6e58671b1a6ab4fcd4a56a81b9c4f2a7cfccbc1af46297f46543a7"
   head "https://github.com/YosysHQ/icestorm.git"
 
   depends_on "pkg-config" => :build
