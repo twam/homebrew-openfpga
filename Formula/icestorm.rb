22f245aae92859aa81d951e142919292e0495921
Tobias Müller <Tobias_Mueller@twam.info>
2023-06-29 15:30:02 +0200

    Update icestorm to 20230218

--- Formula/icestorm.rb
+++ Formula/icestorm.rb
@@ -1,13 +1,13 @@
 class Icestorm < Formula
   desc "Bitstream Tools for Lattice iCE40 FPGAs"
   homepage "http://www.clifford.at/icestorm/"
-  url "https://github.com/YosysHQ/icestorm/archive/2bc541743ada3542c6da36a50e66303b9cbd2059.tar.gz"
-  version "20220603"
-  sha256 "3e47b12a7d6e58671b1a6ab4fcd4a56a81b9c4f2a7cfccbc1af46297f46543a7"
+  url "https://github.com/YosysHQ/icestorm/archive/d20a5e9001f46262bf0cef220f1a6943946e421d.tar.gz"
+  version "20230218"
+  sha256 "19784ecd38faa5284552ade4ca29531a2be1d9227df8956ef35b275c349fdbce"
   head "https://github.com/YosysHQ/icestorm.git"
 
   depends_on "pkg-config" => :build
-  depends_on "libftdi0"
+  depends_on "libftdi"
   depends_on "python"
 
   def install
